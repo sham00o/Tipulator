fe7b8bd092e2e2b4a24502f8b2e48b43ab3de7c6
Samuel Liu <lius94@gmail.com>
2015-12-31 21:26:07 -0600

    pre-submission commit

---
+++ Tipulator/PeopleStack.swift
@@ -0,0 +1,21 @@
+//
+//  PeopleStack.swift
+//  Tipulator
+//
+//  Created by Samuel Liu on 12/29/15.
+//  Copyright © 2015 iSam. All rights reserved.
+//
+
+import UIKit
+
+class PeopleStack: UIStackView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Tipulator/PersonView.swift
@@ -0,0 +1,21 @@
+//
+//  PersonView.swift
+//  Tipulator
+//
+//  Created by Samuel Liu on 12/28/15.
+//  Copyright © 2015 iSam. All rights reserved.
+//
+
+import UIKit
+
+class PersonView: UIImageView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
